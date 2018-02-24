7fd9aeed5d095bac5ffd17c7c7ee44ee9df62950
mtabije <mtabije@gmail.com>
2018-02-24 12:14:01 -0600

    Add files via upload

--- tkgui.py
+++ tkgui.py
@@ -17,17 +17,24 @@ Label(mainWindow, text="Hello2", font=("Calibri", 12)).grid(column=1, row=0)
 
 
 #Text Input
+#Turns out that you should normally seperate creating and object and
+#   setting the grid.  E.g. below someText returns NoneType if the grid
+#   is not run second.
 someText = Entry(mainWindow, width = 20)
+#someText = Entry(mainWindow, width = 20, state='disabled')
 someText.grid(column=1, row=1)
+#cursor will start here
+someText.focus()
 
 def onOkClick():
     howAreYou.configure(text = someText.get()) 
-    print(someText.get())
-    
+    print(someText.get())    
 
 #OK Button
 #okButton = Button(mainWindow, text="OK", font=("Calibri", 12), command=lambda : print("ugggh")).grid(column=0, row=1)
 okButton = Button(mainWindow, text="OK", font=("Calibri", 12), command=lambda : onOkClick()).grid(column=0, row=1)
+
+#ComboBox
 
 
 
