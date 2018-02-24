e4ac99c61cddc5d435380f2f65906964fd80a4f0
mtabije <mtabije@gmail.com>
2018-02-24 10:36:55 -0600

    Add files via upload

--- tkgui.py
+++ tkgui.py
@@ -3,8 +3,8 @@ import sys
 
 #Learning from https://likegeeks.com/python-gui-examples-tkinter-tutorial/
 
+#Create Main Window
 mainWindow = Tk()
-
 mainWindow.title("Whoaaa a window")
 mainWindow.geometry('400x600')
 
@@ -13,8 +13,24 @@ howAreYou = Label(mainWindow, text="Hello", font=("Calibri", 12))
 howAreYou.grid(column=0, row=0)
 
 #Hello 2 Label
-Label(mainWindow, text="Hello2", font=("Calibri", 12)).grid(column=2, row=0)
+Label(mainWindow, text="Hello2", font=("Calibri", 12)).grid(column=1, row=0)
+
+
+#Text Input
+someText = Entry(mainWindow, width = 20)
+someText.grid(column=1, row=1)
+
+def onOkClick():
+    howAreYou.configure(text = someText.get()) 
+    print(someText.get())
+    
 
 #OK Button
-okButton = Button(mainWindow, text="OK", font=("Calibri", 12), command=lambda : print("ugggh")).grid(column=0, row=1)
+#okButton = Button(mainWindow, text="OK", font=("Calibri", 12), command=lambda : print("ugggh")).grid(column=0, row=1)
+okButton = Button(mainWindow, text="OK", font=("Calibri", 12), command=lambda : onOkClick()).grid(column=0, row=1)
+
+
+
+
+#Run the GUI
 mainWindow.mainloop()
